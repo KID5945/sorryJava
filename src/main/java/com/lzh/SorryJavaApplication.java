2630722e3e2d1a08e01d85587fd879546cccb2d9
guomh <guomh@sino-bridge.com>
2020-11-20 10:33:31 +0800

    改为war包打包方式，修改提交路径加上项目路径

--- src/main/java/com/lzh/SorryJavaApplication.java
+++ src/main/java/com/lzh/SorryJavaApplication.java
@@ -2,9 +2,16 @@ package com.lzh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SorryJavaApplication {
+public class SorryJavaApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SorryJavaApplication.class);// 注意 ：这个类必须是启动类的名称
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SorryJavaApplication.class, args);
